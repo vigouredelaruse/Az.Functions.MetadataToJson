53cb2b396ce392d7da6d2e8894f28ab724f3a198
devopswizard <devopswizard@wizardcontroller.com>
2025-01-07 16:13:44 -0500

    scaffolding a console app facade for functions metadata generation

---
+++ Az.Functions.MetadataToJson.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Az.Functions.MetadataToJson;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.CommandLine;
+
+var fileOption = new Option<DirectoryInfo?>(
+    name: "--file",
+    description: "The file to read and display on the console.");
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) =>
+        {
+            var task = new FunctionsMetadataToJson()
+            {
+                OutputDirectory = file == null || file.Exists == false ? "..\\..\\..\\..\\..\\..\\horseless-core-fx\\horselessfx\\horselessnewspaper.workflow.fx.infrastructure\\bin\\net8.0" : file.FullName,
+                          };
+
+            task.Execute();
+        },
+            fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+
--- Az.Functions.MetadataToJson/FunctionsMetadataToJson.cs
+++ Az.Functions.MetadataToJson/FunctionsMetadataToJson.cs
@@ -2,60 +2,66 @@ using System.Text.Json;
 using Az.Functions.MetadataToJson.Models;
 using Microsoft.Build.Framework;
 
-namespace Az.Functions.MetadataToJson;
-
-public class FunctionsMetadataToJson : Microsoft.Build.Utilities.Task
+namespace Az.Functions.MetadataToJson
 {
-    [Required]
-    public string? OutputDirectory { get; set; }
 
-    public override bool Execute()
+    public class FunctionsMetadataToJson : Microsoft.Build.Utilities.Task
     {
-        var functionsMetadataPath = Path.Combine(OutputDirectory!, "functions.metadata");
+        [Required]
+        public string? OutputDirectory { get; set; }
 
-        if (!AssertPathIsValid(OutputDirectory!, functionsMetadataPath))
-            return false;
-
-        var functionsMetadata = GetFunctionsMetadata(functionsMetadataPath);
-
-        ProcessFunctionsMetadata(OutputDirectory!, functionsMetadata);
-
-        return true;
-    }
-
-    private bool AssertPathIsValid(string directoryPath, string functionsMetadataPath)
-    {
-        if (!Directory.Exists(directoryPath))
+        public override bool Execute()
         {
-            Log.LogError("The parameter value provided is not a valid directory path.");
-            return false;
+            var functionsMetadataPath = Path.Combine(OutputDirectory!, "functions.metadata");
+
+            // Log.LogMessage($"working with metadata pathe = {functionsMetadataPath}");
+            if (!AssertPathIsValid(OutputDirectory!, functionsMetadataPath))
+                return false;
+
+            var functionsMetadata = GetFunctionsMetadata(functionsMetadataPath);
+
+            ProcessFunctionsMetadata(OutputDirectory!, functionsMetadata);
+
+            return true;
         }
 
-        if (!File.Exists(functionsMetadataPath))
+        private bool AssertPathIsValid(string directoryPath, string functionsMetadataPath)
         {
-            Log.LogError("The directory does not contain a functions.metadata file.");
-            return false;
+            if (!Directory.Exists(directoryPath))
+            {
+                // Log.LogError($"The parameter value provided is not a valid directory path = {directoryPath}");
+                return false;
+            }
+
+            if (!File.Exists(functionsMetadataPath))
+            {
+                // Log.LogError($"The directory does not contain a functions.metadata file = {functionsMetadataPath}");
+                return false;
+            }
+
+            return true;
         }
 
-        return true;
-    }
-
-    private IEnumerable<FunctionMetadata> GetFunctionsMetadata(string functionsMetadataPath)
-    {
-        var functionsMetadataString = File.ReadAllText(functionsMetadataPath);
-        return JsonSerializer.Deserialize<IEnumerable<FunctionMetadata>>(functionsMetadataString)!;
-    }
-
-    private void ProcessFunctionsMetadata(string directoryPath, IEnumerable<FunctionMetadata> functionsMetadata)
-    {
-        foreach (var functionMetadata in functionsMetadata)
+        private IEnumerable<FunctionMetadata> GetFunctionsMetadata(string functionsMetadataPath)
         {
-            var functionDirectoryPath = Path.Combine(directoryPath, functionMetadata.Name);
-            var functionDirectory = Directory.CreateDirectory(functionDirectoryPath);
-            var functionMetadataFilePath = Path.Combine(functionDirectory.FullName, "function.json");
+            // Log.LogMessage($"working with metadata pathe = {functionsMetadataPath}");
+            var functionsMetadataString = File.ReadAllText(functionsMetadataPath);
+            return JsonSerializer.Deserialize<IEnumerable<FunctionMetadata>>(functionsMetadataString)!;
+        }
 
-            var functionMetadataString = JsonSerializer.Serialize(functionMetadata);
-            File.WriteAllText(functionMetadataFilePath, functionMetadataString);
+        private void ProcessFunctionsMetadata(string directoryPath, IEnumerable<FunctionMetadata> functionsMetadata)
+        {
+            foreach (var functionMetadata in functionsMetadata)
+            {
+                var functionDirectoryPath = Path.Combine(directoryPath, functionMetadata.Name);
+                var functionDirectory = Directory.CreateDirectory(functionDirectoryPath);
+                var functionMetadataFilePath = Path.Combine(functionDirectory.FullName, "function.json");
+
+                var functionMetadataString = JsonSerializer.Serialize(functionMetadata);
+
+                // Log.LogMessage($"writing metadata directory pathe = {functionMetadataFilePath}");
+                File.WriteAllText(functionMetadataFilePath, functionMetadataString);
+            }
         }
     }
 }
